005ce10f6030b9fae5bf4be24572a7dbbb3eca2f
Jonathan Buchanan <johnyburd@gatech.edu>
2019-01-21 12:22:31 -0500

    added name and fixed awful indentation

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -65,18 +65,18 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p2.toString("elee399");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("P3");
-		  toPrint += p3.toString("gburdell3");
-                  //Person4 replace P4 with your name
-                  //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("P4");
-                  toPrint += p4.toString("gburdell4");
-                  //Person5 replace P4 with your name
-                  //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
-		  
-        System.out.println(toPrint);
+		  Person3 p3 = new Person3("Jonathan");
+		  toPrint += p3.toString("jbuchanan41");
+          //Person4 replace P4 with your name
+          //and gburdell4 with your gt id
+          Person4 p4 = new Person4("P4");
+          toPrint += p4.toString("gburdell4");
+          //Person5 replace P4 with your name
+          //and gburdell5 with your gt id
+          Person5 p5 = new Person5("P5");
+          toPrint += p5.toString("gburdell5");
+	        
+          System.out.println(toPrint);
         
         
     }
